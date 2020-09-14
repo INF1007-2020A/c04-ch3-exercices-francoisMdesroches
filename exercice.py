9a91ecc22d49e06d4c455bb1ec68fefa592cca4e
francoisMdesroches <f.deroche@hotmail.com>
2020-09-14 22:49:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,6 +21,7 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     
+    
     return math.radians(angle_degs + (angle_mins/60)+ (angle_secs/60))
 
 
