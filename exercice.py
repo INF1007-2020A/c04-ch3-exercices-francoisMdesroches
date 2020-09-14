f365f0a1b6a6058407833fbb9be928cf0149c7a4
francoisMdesroches <f.deroche@hotmail.com>
2020-09-14 23:25:26 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,7 +26,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     angle_degs = math.degrees(angle_rads)
-    les_degres = math.floor()
+    les_degres = math.floor(angle_degs)
 
     return 0.0, 0.0, 0.0
 
