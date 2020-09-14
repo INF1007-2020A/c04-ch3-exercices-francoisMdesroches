d9e93cc43801d5c84d7d415c8cbda61cabdc44ee
francoisMdesroches <f.deroche@hotmail.com>
2020-09-14 23:28:50 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -30,7 +30,7 @@ def to_degrees(angle_rads: float) -> tuple:
     les_minutes = int((angle_degs-les_degres)*60)
     les_secondes = (angle_degs-les_degres-les_minutes/60)*3600
 
-    return les_, 0.0, 0.0
+    return les_degres, les_minutes, les_secondes
 
 
 def to_celsius(temperature: float) -> float:
